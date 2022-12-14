691ad14d1d3ab22baa68177b7b8de2244da32dc4
Nguyen Duc Tai <67097279+ng-ductai@users.noreply.github.com>
2022-12-14 22:00:49 +0700

    update url

--- admin/src/config.js
+++ admin/src/config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "https://netflix2982.herokuapp.com/api";
+const baseURL = "https://netflix-server.vercel.app/api";
 /* const baseURL = "http://localhost:5800/api"; */
 
 const axiosInstance = axios.create({
