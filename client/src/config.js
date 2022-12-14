8888beafa51da4c66ab8ef15ad3476b2397bdf98
Nguyen Duc Tai <67097279+ng-ductai@users.noreply.github.com>
2022-12-14 22:01:25 +0700

    update url

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://netflix2982.herokuapp.com/api",
+  baseURL: "https://netflix-server.vercel.app/api",
 });
 
 export default axiosInstance;
